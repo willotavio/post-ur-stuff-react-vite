28b17749eec4e765515c8b7da41be9b53a8786db
willotavio <89710130+willotavio@users.noreply.github.com>
2024-12-02 14:51:04 -0300

    feat: add logout function

--- src/services/api/user.ts
+++ src/services/api/user.ts
@@ -50,6 +50,33 @@ export const login = async (user: UserLogin) => {
     return { isSuccesful, statusCode, responseBody }
 }
 
+export const logout = async () => {
+    const response = await fetch(`${import.meta.env.VITE_POSTURSTUFF_API_URI}/user/logout`,
+        {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json, text/plain',
+                'Content-Type': 'application/json;charset=UTF-8'
+            },
+            credentials: "include"
+        }
+    )
+
+    const isSuccesful = response.ok
+    const status = response.status
+
+    var responseBody
+    try {
+        responseBody = await response.json()
+    } catch(error) {
+        responseBody = { error: "Error while trying to process the response body" }
+    }
+
+    return {
+        isSuccesful, status, responseBody
+    }
+}
+
 export const getAllUsers = async () => {
     let response = await fetch(`${import.meta.env.VITE_POSTURSTUFF_API_URI}/user`,
         {
