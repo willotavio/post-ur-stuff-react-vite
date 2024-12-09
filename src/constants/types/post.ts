601aadd25bf2178a7e401508c23c4c3620323c90
willotavio <89710130+willotavio@users.noreply.github.com>
2024-12-09 10:05:10 -0300

    feat: add post types

---
+++ src/constants/types/post.ts
@@ -0,0 +1,24 @@
+import { PostVisibility } from "../enums"
+import { User } from "./user"
+
+export type Post = {
+    id: string,
+    content?: string,
+    images?: string[],
+    user: User,
+    createdAt: Date,
+    editedAt?: Date,
+    visibility: PostVisibility
+}
+
+export type PostAdd = {
+    content?: string,
+    images?: string[],
+    visibility?: PostVisibility
+}
+
+export type PostUpdate = {
+    content?: string,
+    images?: string[]
+    visibility?: PostVisibility
+}
